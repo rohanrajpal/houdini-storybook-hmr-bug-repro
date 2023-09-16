137f8606a695a1fa158086891052f1ae065e944c
rohanrajpal <rohan46000@gmail.com>
2023-09-16 10:04:04 +0530

    add houdini

---
+++ houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+	watchSchema: {
+		url: 'https://graphql.anilist.co/'
+	},
+	plugins: {
+		'houdini-svelte': {}
+	}
+};
+
+export default config;
